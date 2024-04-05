c35d8fafb699f2ba08fd321066539530ca2424a3
markup54 <marzenakupka@tlen.pl>
2024-04-05 11:47:59 +0200

    do dokonczenia zadanie 2

--- app/src/main/java/pl/zabrze/zs10/powtorka3p_piatek/MainActivity2023_styczen.java
+++ app/src/main/java/pl/zabrze/zs10/powtorka3p_piatek/MainActivity2023_styczen.java
@@ -3,12 +3,26 @@ package pl.zabrze.zs10.powtorka3p_piatek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity2023_styczen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2023_styczen);
+        ArrayList<String> todoList = new ArrayList<>();
+        todoList.add("Posprzatac");
+        todoList.add("Zakupy");
+        todoList.add("Kino");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,todoList);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 }
